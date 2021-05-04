9f466351905f85a2f0d7f30e8ac8a134dfd54a24
Marion Desbas <desbasmarion@yahoo.fr>
2021-05-04 18:12:54 +0200

    first commit

---
+++ starterOnly/modal.js
@@ -0,0 +1,27 @@
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const closeModal = document.querySelector(".close");
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+// close modal form on click
+closeModal.addEventListener("click", (e)=>{
+  modalbg.style.display = "none";
+});
